d972f448393dd3687d54c233928abfcaae327c6c
mrtnmmn <mrtn.lmba@gmail.com>
2023-07-23 13:20:14 +0200

    first commit

---
+++ src/Components/MainContents/MainContents.js
@@ -0,0 +1,20 @@
+import headerPicture from '../../resources/IMG_1568.JPG'
+import './MainContents.css'
+function MainContents() {
+  return (  
+    <div className="mainDiv">
+      <img src={headerPicture} className='headerImage' />
+      a
+      <img src={headerPicture} className='headerImage' />
+
+      <img src={headerPicture} className='headerImage' />
+
+      <img src={headerPicture} className='headerImage' />
+
+      <img src={headerPicture} className='headerImage' />
+
+    </div>
+  );
+}
+
+export default MainContents;
---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,12 @@
+import './Sidebar.css'
+
+function Sidebar() {
+  return (  
+    <div className="mainSideBarDiv">
+      Sidebar
+      <div>Click</div>
+    </div>
+  );
+}
+
+export default Sidebar;
