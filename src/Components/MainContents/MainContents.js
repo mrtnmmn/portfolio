1d8e5c675c6a863c46ccaa8d72276c0e91283fe9
mrtnmmn <mrtn.lmba@gmail.com>
2023-07-26 01:31:03 +0200

    added description paragraph

--- src/Components/MainContents/MainContents.js
+++ src/Components/MainContents/MainContents.js
@@ -13,9 +13,16 @@ function MainContents() {
           {t('jobTitle')}
         </div>
       </div>
-      <div className='description'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+      <div className='sectionTitle'>
+        {t('aboutMeSectionTitle')}
       </div>
+      <p className='description'>
+        {t('descriptionParagraph1')}
+      </p>
+      <p className='description'>
+        {t('descriptionParagraph2')}
+      </p>
     </div>
   );
 }
